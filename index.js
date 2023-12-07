2e209155dd956135ad32b8722c39e66586a915d9
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-12-07 16:10:04 -0500

    updated index.js with functions and questions; updated the renderLicenseBadge function

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
-// TODO: Include packages needed for this application
+//  packages
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+/*
+
+WHEN I choose a license for my application from a list of options -- will be a dropdown:
+THEN a and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+
+ */
+
+// user input questions
+const questions = [
+  {
+    type: "input",
+    message: "What is the project title?",
+    name: "title",
+  },
+  {
+    type: "editor",
+    message: "What is the project description?",
+    name: "description",
+  },
+  {
+    type: "editor",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description.",
+    name: "installation",
+  },
+  {
+    type: "editor",
+    message: "Provide instructions and examples for your application use.",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your project:",
+    choices: ["No License", "Apache", "IBM", "MIT", "Perl"],
+    name: "license",
+  },
+  {
+    type: "editor",
+    message:
+      "Write tests for your application and provide examples on how to run them here.",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err=>{
+        if(err) throw err
+        console.log("complete!");
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// app initialization function
+function init() {
+  inquirer.prompt(questions)
+  .then(data=>{
+    writeToFile("./output/README.md", data)
+  })
+}
 
-// Function call to initialize app
+// Function call
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "IBM") {
+    return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+  } else if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Perl") {
+    return `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`;
+  } else if (license === "No License") {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +26,47 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  ${renderLicenseSection(data)}
+  
+  ## Contributions
+  
+ ${data.contribution}
+  
+  ## Tests
+  
+  ${data.test}
+  
+  ## Questions
+  
+  <ul> 
+  <a href ="https://github.com/${data.username}/"> GitHub Profile</a> 
+  <a href ="mailto:${data.email}"> Email Me </a>
+  </ul>
 `;
 }
 
